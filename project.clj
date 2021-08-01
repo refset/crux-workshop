6befe4dd917994fd4571796a6ba034994f2d1036
Jeremy Taylor <jdt@juxt.pro>
2021-08-01 17:17:19 +0100

    upgrade deps

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [juxt/crux-core "20.09-1.12.0-beta"] ;; added these two
-                 [juxt/crux-rocksdb "20.09-1.12.0-beta"]
-                 [juxt/crux-http-server "20.09-1.12.0-alpha"]] ;; but otherwise is vanilla "lein new app myproject"
+                 [pro.juxt.crux/crux-core "1.18.0"] ;; added these
+                 [pro.juxt.crux/crux-rocksdb "1.18.0"]
+                 [pro.juxt.crux/crux-lucene "1.18.0"]
+                 [pro.juxt.crux/crux-http-server "1.18.0"]] ;; but otherwise is vanilla "lein new app myproject"
   :main ^:skip-aot myproject.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
